7f25b38adc6f2672759fec72c13aa6456dfb7108
raven666721853 <takihamdi027@gmail.com>
2025-10-20 11:59:45 +0000

    ajout fichiers valves

---
+++ backend/models/Valve.js
@@ -0,0 +1,9 @@
+import mongoose from "mongoose";
+const valveSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  state: { type: String, enum: ["on", "off"], default: "off" },
+  location: String,
+  farm: { type: mongoose.Schema.Types.ObjectId, ref: "Farm", required: true },
+});
+export default mongoose.model("Valve", valveSchema);
+//valve
